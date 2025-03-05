d1d2ef35174ee2d3749f813522152c94e3106ee8
lakrounehamza <lakrounehamza10@gmail.com>
2025-03-05 02:22:07 +0100

    ajoute le modèle Trajet et les relations entre les classes

--- app/Models/Trajet.php
+++ app/Models/Trajet.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Trajet extends Model
 {
-    //
+    protected $fillable = ['statut','depart','arrive','id_chauffeur'];
+
+    public function    user(){
+        return $this->belongsTO(User::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,13 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+    public function trajet(){
+        return $this->hasMany(Trajet::class);
+    }
+
+    public  function reservation(){
+        return   $this->hasMany(Reservation::class);
+    }
 }
